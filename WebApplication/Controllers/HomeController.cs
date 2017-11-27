19361bf66c782b25528567633ddb784eef92337a
wtoonen <willem_toonen@hotmail.com>
2017-11-27 16:38:22 +0100

    --enviroment development

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -29,6 +29,13 @@ namespace WebApplication.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            ViewData["Message"] = "Your test page.";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
